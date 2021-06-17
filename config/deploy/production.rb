128a6827baacd0355652c90c7aba69b67cf6c5b6
“mbartlett413” <Mbartlett413@me.com>
2021-06-17 18:23:48 -0400

    typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '157.230.13.169', user: 'augustus', roles: %w{app db web}
+server '157.230.13.169', user: 'alpha', roles: %w{app db web}
 
 # role-based syntax
 # ==================
