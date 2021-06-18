663fbff253a057fdaf43348424c3dce0e60627b4
“mbartlett413” <Mbartlett413@me.com>
2021-06-18 12:01:42 -0400

    production

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@github.com:Resource-Stack/lsa-demo.git"
 
 #https://github.com/Mbartlett413/portfolioTwo
 # Deploy to the user's home directory
-set :deploy_to, "/home/alpha/#{fetch :application}"
+set :deploy_to, "/home/augustus/dev/#{fetch :application}"
 
 set :linked_files, %w{config/master.key}
 
@@ -18,7 +18,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 
-set :passenger_restart_with_touch, false
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
