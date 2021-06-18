7a016c82cc21fb02125f17e19a16549ac6ae2286
“mbartlett413” <Mbartlett413@me.com>
2021-06-17 22:11:01 -0400

    passenger config

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 
+set :passenger_restart_with_touch, false
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
